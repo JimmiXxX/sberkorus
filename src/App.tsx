fe27d0e892398997bd2f3b9abbc59d69e27aa21a
petrov.i <petrov.i@cpateam.xyz>
2024-08-24 17:26:38 +0300

    change div to ul+li

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React , { useState } from 'react';
 import Timeline from './components/Timeline';
-import { Simulate } from "react-dom/test-utils";
-import input = Simulate.input;
 
 const App = () => {
 
     const [step , setStep] = useState<number> (5)
     const [completed , setCompleted] = useState<number> (2)
     const [progress , setProgress] = useState<boolean> (true)
-    const [xPos , setXPos] = useState<string> ("point")
+    const [xPos , setXPos] = useState<string> ('point')
     const [yPos , setYPos] = useState<string> ('bottom')
 
     const incrementStep = () => {
@@ -26,30 +24,49 @@ const App = () => {
 
     return (
         <div className="app">
-            <Timeline step={ step } completed={ completed } inProgress={ progress } xPosition={xPos} yPosition={yPos}/>
+            <Timeline step={ step } completed={ completed } inProgress={ progress } xPosition={ xPos }
+                      yPosition={ yPos }/>
 
-            <div className="buttons">
-                <div>
+            <ul className="buttons">
+                <li>
                     <button onClick={ incrementStep }>Увеличить шаг</button>
                     <button onClick={ decrementStep }>Уменьшить шаг</button>
-                </div>
-                <div>
+                </li>
+                <li>
                     <button onClick={ incrementCompleted }>Увеличить выполненных</button>
                     <button onClick={ decrementCompleted }>Уменьшить выполненных</button>
-                </div>
-                <div>
-                    <button onClick={ ()=>{setXPos('point')} }>На точке</button>
-                    <button onClick={ ()=>{setXPos('line')} }>На линии</button>
-                </div>
-                <div>
-                    <button onClick={ ()=>{setYPos('top')} }>Сверху</button>
-                    <button onClick={ ()=>{setYPos('bottom')} }>Снизу</button>
-                </div>
-                <div>
-                    <button onClick={ ()=>{setProgress(true)} }>Прогресс</button>
-                    <button onClick={ ()=>{setProgress(false)} }>Нет прогресса</button>
-                </div>
-            </div>
+                </li>
+                <li>
+                    <button onClick={ () => {
+                        setXPos ('point')
+                    } }>На точке
+                    </button>
+                    <button onClick={ () => {
+                        setXPos ('line')
+                    } }>На линии
+                    </button>
+                </li>
+                <li>
+                    <button onClick={ () => {
+                        setYPos ('top')
+                    } }>Сверху
+                    </button>
+                    <button onClick={ () => {
+                        setYPos ('bottom')
+                    } }>Снизу
+                    </button>
+                </li>
+                <li>
+                    <button onClick={ () => {
+                        setProgress (true)
+                    } }>Прогресс
+                    </button>
+                    <button onClick={ () => {
+                        setProgress (false)
+                    } }>Нет прогресса
+                    </button>
+                </li>
+            </ul>
         </div>
 
     );
